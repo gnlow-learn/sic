eacd16b9956a40c90575e59ceba3b0731a146c55
Gnlow <gnlow@pm.me>
2025-04-11 16:36:24 +0900

    J is easier than JSUB?

--- src/func.sic.asm
+++ src/func.sic.asm
@@ -2,16 +2,33 @@ stack	START	0
 	LDA	#myst
 	JSUB	stinit
 
-	LDA	#0x77
+	LDA	#0x11
 	JSUB	push
 	
-	LDA	#0x77
+	LDA	#0x33
 	JSUB	push
 	
-	LDA	#0x77
-	JSUB	push
+	JSUB	myadd
+jj	TIO
 
 halt	J	halt
+
+... custom functions
+
+myadd	JSUB	pop
+	STA	foo
+	JSUB	pop
+	STA	bar
+
+	LDA	foo
+	ADD	bar
+
+	J	#jj
+
+foo	RESW	1
+bar	RESW	1
+
+... stack
 
 stinit	STA	sp
 	RSUB
@@ -29,6 +46,8 @@ pop	LDA	sp
 	RSUB
 
 sp	RESW	1
+
+...
 
 myst	RESB	4096
 
