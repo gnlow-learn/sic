1d6f92cba1a74ed7fc2c12a90da308ef2865348a
Gnlow <gnlow@pm.me>
2025-04-13 10:33:49 +0900

    Update hello.sic.asm

--- src/hello.sic.asm
+++ src/hello.sic.asm
@@ -1,18 +1,27 @@
-prog START   0
+. Program
+. . Statement
+. . . ExpressionStatement
+. . . . Expression
+. . . . . FunctionCall
+. . . . . . Identifier
+. . . . . . Expression
+. . . . . . . String
 
-        LDA	#myst
-        JSUB    stinit
-        JSUB    stinitr
+
+prog	START	0
+	LDA	#myst
+	JSUB	stinit
+	JSUB	stinitr
 
 ... main ...
-        LDA     #vva
-        JSUB    push
-        JSUB    pushr
-        JSUB    print
+	LDA	#vva
+	JSUB	push
+	JSUB	pushr
+	JSUB	print
 halt	J	halt
 
 ... table ...
-vva     BYTE    C'Hello, World!;'
+vva	BYTE	C'Hello, World!;'
 
 ... stdio ...
 print	JSUB	pop
@@ -31,48 +40,47 @@ ploop	TD	#1
 prp	RESW	1
 
 ... stdlib ...
-
-myst    RESW    100
+myst	RESW	100
 ...
-stinit  STA     sp
-        RSUB
+stinit	STA	sp
+	RSUB
 
-push    STA     @sp
-        LDA     sp
-        ADD     #3
-        STA     sp
-        RSUB
+push	STA	@sp
+	LDA	sp
+	ADD	#3
+	STA	sp
+	RSUB
 
-pop     LDA     sp
-        SUB     #3
-        STA     sp
-        LDA     @sp
-        RSUB
+pop	LDA	sp
+	SUB	#3
+	STA	sp
+	LDA	@sp
+	RSUB
 
-sp      RESW    1
+sp	RESW	1
 
 ...
-stinitr LDA     #retadr
-        STA     spr
-        RSUB
+stinitr	LDA	#retadr
+	STA	spr
+	RSUB
 
-pushr   STL     @spr
-        LDA     @spr
-        ADD     #3
-        STA     @spr
+pushr	STL	@spr
+	LDA	@spr
+	ADD	#3
+	STA	@spr
 
-        LDA     spr
-        ADD     #3
-        STA     spr
-        RSUB
+	LDA	spr
+	ADD	#3
+	STA	spr
+	RSUB
 
-popr    LDA     spr
-        SUB     #3
-        STA     spr
-        LDL     @spr
-        RSUB
+popr	LDA	spr
+	SUB	#3
+	STA	spr
+	LDL	@spr
+	RSUB
 
-spr     RESW    1
-retadr  RESW    100
+spr	RESW	1
+retadr	RESW	100
 
-        END     prog
+	END	prog
